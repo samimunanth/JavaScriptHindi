c7ab1086a05cd1b5a400dd420e6b67cdb6c58979
Samim Anvar <samim.anvar323@gmail.com>
2024-02-29 13:40:29 +0000

    conversation operaion

--- 01_basics/03_conversitionOperation.js
+++ 01_basics/03_conversitionOperation.js
@@ -21,6 +21,46 @@ let valueInNumber=Number(scroe)
 
  let someNumner=33
  let stringNumber=String(someNumner)
- console.log(stringNumber)
- console.log(typeof stringNumber)
+//  console.log(stringNumber)
+//  console.log(typeof stringNumber)
 
+
+ //*************Operations*** */
+
+ let value =3
+ let negValuie=-value
+ //console.log(negValuie)
+
+//  console.log(2+3);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**2);
+//  console.log(2/3);
+//  console.log(2%3);
+
+let str1 = "hello"
+let str2 =" samim"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" +2) //12
+// console.log(1 +"2")//12
+// console.log("1" +"2" + 2)//122
+// console.log(1 +2 + "2")//122
+
+// console.log((3+4) * 5 % 3)
+
+// console.log(+true) //1
+// console.log(+"") //0
+
+let num1 ,num2, num3
+
+num1=num2=num3=2+2
+console.log(num1)
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter)
+
+//documentation
+//https://tc39.es/ecma262/ 
