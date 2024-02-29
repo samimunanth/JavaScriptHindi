ae2baeee451135332ec11d2fcdc2b053af162718
Samim Anvar <samim.anvar323@gmail.com>
2024-02-29 12:01:38 +0000

    convertion operation

---
+++ 01_basics/03_conversitionOperation.js
@@ -0,0 +1,26 @@
+let scroe=true
+
+// console.log(typeof scroe)
+// console.log(typeof(scroe))
+
+let valueInNumber=Number(scroe)
+
+// console.log(valueInNumber)
+
+// "33" => number
+// "33abc" => Nan 
+// true => 1; false=>0
+ let isLoggedIn="Samim"
+ let booleanIsLoggedIn= Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+ // 1=> true; 0=> false
+ //""=> false
+ // "samim"=> true
+
+
+ let someNumner=33
+ let stringNumber=String(someNumner)
+ console.log(stringNumber)
+ console.log(typeof stringNumber)
+
